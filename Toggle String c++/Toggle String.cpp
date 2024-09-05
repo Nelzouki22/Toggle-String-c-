07e4b0eeba49a6ec6521171202a09359b10b7865
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-05 17:45:19 -0400

    Add project files.

---
+++ Toggle String c++/Toggle String.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // قراءة السلسلة النصية
+    string S;
+    cin >> S;
+
+    // تغيير حالة الأحرف
+    for (int i = 0; i < S.length(); i++) {
+        if (islower(S[i])) {
+            S[i] = toupper(S[i]);
+        }
+        else if (isupper(S[i])) {
+            S[i] = tolower(S[i]);
+        }
+    }
+
+    // طباعة السلسلة الناتجة
+    cout << S << endl;
+
+    return 0;
+}
